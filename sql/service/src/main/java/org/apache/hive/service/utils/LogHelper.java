1cb32373df570d039922bd3673a7fd1a311deac5
angerszhu <angers.zhu@gmail.com>
2019-11-05 15:03:56 +0800

    update license

--- sql/service/src/main/java/org/apache/hive/service/utils/LogHelper.java
+++ sql/service/src/main/java/org/apache/hive/service/utils/LogHelper.java
@@ -1,15 +1,19 @@
 /**
- * This class provides helper routines to emit informational and error
- * messages to the user and log4j files while obeying the current session's
- * verbosity levels.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * NEVER write directly to the SessionStates standard output other than to
- * emit result data DO use printInfo and printError provided by LogHelper to
- * emit non result data strings.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * It is perfectly acceptable to have global static LogHelper objects (for
- * example - once per module) LogHelper always emits info/error to current
- * session as required.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.hive.service.utils;
