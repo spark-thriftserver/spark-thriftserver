2df4fe6ba36186a062bf476e55005fb679aa4601
AngersZhuuuu <angers.zhu@gmail.com>
2019-12-13 14:53:48 +0800

    file -> processor (#62)

--- sql/service/src/main/java/org/apache/spark/sql/service/cli/processor/SparkFileProcessor.java
+++ sql/service/src/main/java/org/apache/spark/sql/service/cli/processor/SparkFileProcessor.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.spark.sql.service.cli.file;
+package org.apache.spark.sql.service.cli.processor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
